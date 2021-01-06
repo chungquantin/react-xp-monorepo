45faa4a2de92fbd04008624ca8dc50fbffcba3aa
Chung Quan Tin <s3818487@rmit.edu.vn>
2021-01-06 17:31:38 +0700

    add native and web

---
+++ package/web/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import s from "@monorepo/shared";
+
+function App() {
+	return <div className="App">{s}</div>;
+}
+
+export default App;
